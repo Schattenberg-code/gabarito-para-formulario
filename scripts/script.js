bf52a3e2f0c54faab8552156ce011d6845a76f3d
Schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-04-10 10:08:10 -0300

    inicial commit

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+let cadastro = false;
+
+document.getElementById("toggle").onclick = () =>{
+
+    cadastro = !cadastro;
+
+    document.getElementById("title").innerText = cadastro ? "-Cadastro-" : "-Login-";
+    document.querySelector("button").innerText = cadastro ? "Cadastrar" : "Entrar";
+
+    document.getElementById("toggle").innerText = cadastro 
+    ? "Ja tem conta? faça login!" 
+    : "Não tem conta? Cadastre-se!";
+
+    document.getElementById("mensagem").innerText = "";
+}
+
+    document.getElementById("form-login").onsubmit = (e) =>{
+
+        e.preventDefault();
+
+        let email = document.getElementById('email').value;
+        let senha = document.getElementById('senha').value;
+        let mensagem = document.getElementById('mensagem');
+
+        mensagem.innerHTML = "";
+
+        if (!email.includes("@") || (!email.includes("."))){
+            mensagem.innerHTML = "<div class='erro'><p>Email invalido!</p></div>";
+            return;
+        }
+
+        if (senha.length < 4){
+            mensagem.innerHTML = "<div class='erro'><p>Senha muito curta!</p></div>";
+            return;
+        }
+
+        if(cadastro){
+            localStorage.setItem(email, senha);
+            mensagem.innerHTML = "<div class='sucesso'><p>Cadastrado com sucesso!</p></div>";
+        } else {
+
+            let salva = localStorage.getItem(email);
+
+            if(salva === senha){
+                mensagem.innerHTML = "<div class='sucesso'><p>logado com sucesso</p></div>";
+            } else {
+                mensagem.innerHTML = "<div class='sucesso'><p>dados invalidos!</p></div>";
+            }
+        }
+
+        document.getElementById("form-login").reset;
+    }
